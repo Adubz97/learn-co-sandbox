f9ffe37eb0fe45a031966572ada349e3968879bd
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2021-03-03 04:52:17 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,14 @@
+# please dont do this :)
+puts (1#+ 1)
+
+#perform a constant expression evaluation
+10
+#Assign constant 10 to bare-word variable triangle_sides
+'triangle_sides'
+
+#from the three dog night song:"joy to the world (jeremiah was a bullfrog)"
+puts "joy to the world"
+puts "All the boys and girls"
+# puts "joy to the fishes in the deep blue sea"
+puts "joy to you and me"
+
